26432080ad3c8d2534c2656b83189d08b220659e
miagomezch <miagomezch@unal.edu.co>
2019-04-21 16:09:15 -0500

    Updates

---
+++ precache-manifest.37c13892075bf2533c9027c834a43adb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "10fb711b9860e8a449c1e697c96314e7",
+    "url": "/regist-user/static/media/logo.10fb711b.png"
+  },
+  {
+    "revision": "b3f4801cdb1f18f0f32803167742844c",
+    "url": "/regist-user/static/media/headerBanner.b3f4801c.jpg"
+  },
+  {
+    "revision": "b3c457b9dad944f205f80b1a05c80c50",
+    "url": "/regist-user/static/media/escudo.b3c457b9.png"
+  },
+  {
+    "revision": "fc4647071d4cb1dbd31c389f66456e42",
+    "url": "/regist-user/static/media/LOGO_EJC.fc464707.png"
+  },
+  {
+    "revision": "0cd27456e3522515543c",
+    "url": "/regist-user/static/js/runtime~main.1fc1cd1c.js"
+  },
+  {
+    "revision": "339f5d5cca3dad0ed391",
+    "url": "/regist-user/static/js/main.d3002875.chunk.js"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/regist-user/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "339f5d5cca3dad0ed391",
+    "url": "/regist-user/static/css/main.872e1fa3.chunk.css"
+  },
+  {
+    "revision": "772964fa099dea31b91debdf81667d90",
+    "url": "/regist-user/index.html"
+  }
+];
